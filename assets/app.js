9734c2b08ce354c7dc9cec1e1d9393ee1c55aef0
Niki4507 <niki.tidmore@gmail.com>
2017-10-10 09:44:21 -0500

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,21 @@
 var facts = [
     "Ampere is the unit of measuring currents",
-    "Alessandro Volta invented the first electric circuit",
+    "Alessandro Volta invented the first electric circuit and the first battery",
     "Open switches allow electricity to flow through",
     "Electricity was discovered by the ancient Greeks",
     "Electric eels can produce shocks of around 500 volts", 
     "Muscle cells in the heart are contracted by electricity",
     "Benjamin Franklin invented the lightning rod to protect buildings",
+    "Thomas Edison owned the first power plant, Pearl Street Station in Manhattan, which was built in 1982.",
+    "Christmas tree lights stay lit when a bulb breaks because they are on parallel circuits",
+    "The world's biggest lightbulb is in Edison, NJ",
+    "Iceland uses the most electricity each year",
+    "Electricity travels at 6,696,000 mph",
+    "William Morrison built the first electric car",
+    "Electricity in Albertville, France is produced by cheese",
+    "Most of Quebec's electricity is produce from water - 96% is from hydroelectricity",
+    "Electric eels produce around 500 volts of electricity and use it for hunting and self defense",
+    "Michael Faraday is known as the Father of Electricity",
 
 ];
 
